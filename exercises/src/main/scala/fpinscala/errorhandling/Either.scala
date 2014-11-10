91e920b70071b74de7fa95ba2fa7106f6231cbe1
Galder Zamarreño <galder@zamarreno.com>
2014-11-10 17:35:04 +0100

    Exercise 4.7

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -48,4 +48,14 @@ object Either {
     try Right(a)
     catch { case e: Exception => Left(e) }
 
+  def map2[A,B,C,E](a: Either[E, A], b: Either[E, B])(f: (A, B) => C): Either[E, C] =
+    a.flatMap(x => b.map(y => f(x, y)))
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    // es.foldLeft[Either[E, List[A]]](Right(Nil))((acc, x) => acc.flatMap(l => x.map(l :+ _)))
+    es.foldLeft[Either[E, List[A]]](Right(Nil))((acc, x) => map2(acc, x)(_ :+ _))
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    as.foldLeft[Either[E, List[B]]](Right(Nil))((acc, x) => map2(acc, f(x))(_ :+ _))
+
 }
