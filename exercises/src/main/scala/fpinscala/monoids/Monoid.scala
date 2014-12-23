b2ba8349cf05a6d0c2cbcf8c29e607084bb8c4d9
Galder Zamarreño <galder@zamarreno.com>
2014-12-23 10:21:33 +0100

    10.2 - Monoid for Option

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -40,7 +40,10 @@ object Monoid {
     val zero = true
   }
 
-  def optionMonoid[A]: Monoid[Option[A]] = sys.error("todo")
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    def op(a1: Option[A], a2: Option[A]): Option[A] = a1.orElse(a2)
+    def zero = None
+  }
 
   def endoMonoid[A]: Monoid[A => A] = sys.error("todo")
 
